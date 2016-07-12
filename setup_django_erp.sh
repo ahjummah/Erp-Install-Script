676a611580cc40b01070f66d124c36a789fdb930
jessacabigas <jessacabigas@gmail.com>
2016-07-12 14:40:09 +0800

    Added setup_django_erp.sh file

---
+++ setup_django_erp.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+get_mariadb(){
+  cd ..
+  sudo apt-get update
+  sudo apt-get install python-pip python-dev mariadb-server libmariadbclient-dev libssl-dev
+  sudo mysql_secure_installation
+}
+create_database(){
+  mysql -u root -p -e "CREATE DATABASE erpnext_django CHARACTER SET UTF8;
+  CREATE USER Administrator@localhost IDENTIFIED BY 'password';
+  GRANT ALL PRIVILEGES ON erpnext_django.* TO Administrator@localhost;
+  FLUSH PRIVILEGES;
+  exit"
+}
+
+set_app(){
+  cd ~/New-ERP
+  pip install -r requirements.txt
+  echo "Done installing requirements"
+}
+
+configure_django_db(){
+    # cd ~/ERP_Project
+  echo "$PWD"
+  echo "Making migrations...."
+  python manage.py makemigrations
+  python manage.py migrate
+}
+
+create_superuser(){
+  # cd ~/erpnext_django
+  echo "Creating superuser...."
+  python manage.py createsuperuser
+}
+
+install_frappe(){
+  echo "$PWD"
+  cd ..
+  wget https://raw.githubusercontent.com/jessacabigas/bench-mirror/master/install_scripts/setup_frappe.sh
+  sudo bash setup_frappe.sh --setup-production
+}
+
+main(){
+  get_mariadb
+  create_database
+  set_app
+  configure_django_db
+  create_superuser
+  mysql -u Administrator -p erpnext_django < erpnext_django.sql
+  install_frappe
+}
+main $@
